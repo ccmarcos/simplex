7f3b38ecf8347698f1b0fe005d3c3331db9c9c0f
ccmarcos <marcosantonio199189@gmail.com>
2017-04-27 00:49:03 -0500

    finish input dynamic

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,18 +1,55 @@
 function newInput() {
+    var c=1;
+    var array = ["<=", "=", ">="];
     var num_variables_decision = document.getElementById("num_variables_decision").value;
+    var num_restricciones = document.getElementById("num_restricciones").value;
     var inpt = document.createElement('input');
+    var selectList = document.createElement('select');
     document.f1.innerHTML += "<strong>Z=</strong>";
 
     if (num_variables_decision) {
         for (var i = 0; i < num_variables_decision; i++) {
-          var inpt = document.createElement('input');
           inpt.type = "text";
-          inpt.name = "input_" + i;
-          inpt.id = "input_" + i;
+          inpt.name = "objetivo" + i;
+          inpt.id = "objetivo" + i;
           document.f1.appendChild(inpt);
           if(i!=num_variables_decision-1)
             document.f1.innerHTML += "<strong>X"+(i+1)+"+ </strong>";
           else document.f1.innerHTML += "<strong>X"+(i+1)+"</strong>";
         }
+        document.f1.innerHTML += "</br>"
+    }
+
+    if(num_restricciones){
+      for(var i = 0; i< num_restricciones; i++){
+        for(var j=0; j< parseInt(num_variables_decision)+1; j++){
+          inpt.type = "text";
+          inpt.name = "input_"+j+"_"+i;
+          inpt.id = "input_"+j+"_"+i;
+          document.f1.appendChild(inpt);
+          if(j<num_variables_decision-1)
+            document.f1.innerHTML += "<strong>X"+(j+1)+"+</strong>";
+            else if(j<num_variables_decision){
+              document.f1.innerHTML +="<strong>X"+(j+1)+"</strong>";
+            selectList.id = "input_"+j+"_"+i;
+            document.f1.appendChild(selectList);
+            }
+
+          if(j == num_variables_decision  && c==1){
+            for (var k = 0; k < array.length; k++) {
+                    var option = document.createElement("option");
+                    option.value = array[k];
+                    option.text = array[k];
+                    selectList.appendChild(option);
+                }
+                c=0;
+        }
+
+        document.f1.appendChild(inpt);
+        }
+
+
+        document.f1.innerHTML += "</br>"
+      }
     }
 }
