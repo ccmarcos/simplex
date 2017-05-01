6eef89eab991f2f153572c85f1e43b3380d2ee9c
ccmarcos <marcosantonio199189@gmail.com>
2017-05-01 03:10:43 -0500

    first table

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,4 @@
+var matriz = new Array();
 function newInput() {
     var c=1;
     var array = ["<=", "=", ">="];
@@ -44,7 +45,6 @@ function newInput() {
                 }
                 c=0;
         }
-
         document.f1.appendChild(inpt);
         }
         document.f1.innerHTML += "</br>";
@@ -58,8 +58,11 @@ function ecuacion(){
   var num_variables_decision = document.getElementById("num_variables_decision").value;
   var num_restricciones = document.getElementById("num_restricciones").value;
   var seleccion;
-  var matriz = new Array();
   var contador_seleccion=0;
+  var posicionExcHolgAr=parseInt(num_variables_decision);
+  var posicionArtif=new Array();
+  var horizontal = new Array();
+  var suma;
 
   for(var i=0; i<num_restricciones; i++){
     seleccion = document.getElementById("input_"+i).value;
@@ -68,19 +71,72 @@ function ecuacion(){
     else contador_seleccion+=2;
   }
 
-  for(var i=0; i<parseInt(num_restricciones)+1;i++)
-      matriz[i]= new Array();
+  crearMatriz(num_restricciones);
+  inicializaMatriz(num_restricciones,num_variables_decision,contador_seleccion);
 
-  for(var i=0; i<parseInt(num_restricciones)+1;i++)
-    for(var j=0; j<parseInt(num_variables_decision)+parseInt(contador_seleccion)+2;j++)
-      matriz[i][j]=0;
-
-  for(var i=0; i<parseInt(num_restricciones)+1; i++){
-    for(var j=0; j<parseInt(num_variables_decision)+parseInt(contador_seleccion)+2; j++){
-      prueba.innerHTML+=matriz[i][j];  
+  for(var i=0; i<num_restricciones; i++){
+    for(var j=0; j< parseInt(num_variables_decision)+1; j++){
+        if(j==num_variables_decision)
+          matriz[i+1][parseInt(num_variables_decision)+parseInt(contador_seleccion)+1]=document.getElementById("input_"+j+"_"+i).value;
+        else matriz[i+1][j+1]=document.getElementById("input_"+j+"_"+i).value;
     }
-      prueba.innerHTML+="\n";
-
   }
 
+  posicionArtif.push(0);
+  for(var i=0; i<num_restricciones; i++){
+    seleccion = document.getElementById("input_"+i).value;
+    if(seleccion == "<="){
+      posicionExcHolgAr+=1;
+      matriz[i+1][posicionExcHolgAr]=1;
+    }
+    else if(seleccion=="="){
+      posicionExcHolgAr+=1;
+      matriz[i+1][posicionExcHolgAr]=1;
+      matriz[0][posicionExcHolgAr]=-1;
+      posicionArtif.push(i+1);
+    }
+    else {
+      posicionExcHolgAr+=1;
+      matriz[i+1][posicionExcHolgAr]=-1;
+      posicionExcHolgAr+=1;
+      matriz[i+1][posicionExcHolgAr]=1;
+      matriz[0][posicionExcHolgAr]=-1;
+      posicionArtif.push(i+1);
+    }
+  }
+  matriz[0][0]=1;
+
+  for(var i=0; i< parseInt(num_variables_decision)+parseInt(contador_seleccion)+2; i++){
+    suma=0;
+    for(var j=0; j<posicionArtif.length; j++){
+      suma+=parseInt(matriz[posicionArtif[j]][i]);
+    }
+  //  horizontal.push(suma);
+    matriz[0][i]=suma;
+  }
+
+  //for(var i=0; i< parseInt(num_variables_decision)+parseInt(contador_seleccion)+2; i++)
+  //  prueba.innerHTML+=horizontal[i];
+  //prueba.innerHTML+=posicionExcHolgAr;
+  imprimeMatriz(num_restricciones,num_variables_decision,contador_seleccion);
+}
+
+function crearMatriz(restric){
+  for(var i=0; i<parseInt(restric)+1;i++)
+      matriz[i]= new Array();
+}
+
+function inicializaMatriz(restric, varDecision, contaSelect){
+  for(var i=0; i<parseInt(restric)+1;i++)
+    for(var j=0; j<parseInt(varDecision)+parseInt(contaSelect)+2;j++)
+      matriz[i][j]=0;
+}
+
+function imprimeMatriz(restric, varDecision, contaSelect){
+  for(var i=0; i<parseInt(restric)+1; i++){
+    for(var j=0; j<parseInt(varDecision)+parseInt(contaSelect)+2; j++){
+      prueba.innerHTML+=matriz[i][j];
+    }
+      prueba.innerHTML+="\n";
+  }
 }
